bc6634ddf6337ef4c707e17862d171e33a81d03e
Linus J <30862662+linus-jansson@users.noreply.github.com>
2024-04-26 21:20:23 +0200

    stuff

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # teamspeak
+
+
+docker run -d --name TeamSpeak3_Server -e TIME_ZONE=Europe/Berlin -p 9987:9987/udp -p 10011:10011/tcp -p 30033:30033/tcp -v ts3:/teamspeak/save/ ertagh/teamspeak3-server:latest
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Run teamspeak
-docker run -p 9987:9987/udp -p 10011:10011 -p 30033:30033 -e TS3SERVER_LICENSE=accept teamspeak
+docker run -p 9987:9987/udp -p 10011:10011 -p 30033:30033 -e TS3SERVER_LICENSE=accept ertagh/teamspeak3-server:latest
